0814e781fd85e53d3c609102ef8aade900392aac
MinaDoncheva <mina.doncheva@icloud.com>
2024-02-05 19:17:57 +0200

    HomePage test passes

--- application/src/test/java/com/codbex/atlas/ui/tests/HomePageIT.java
+++ application/src/test/java/com/codbex/atlas/ui/tests/HomePageIT.java
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2022 codbex or an codbex affiliate company and contributors
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-FileCopyrightText: 2022 codbex or an codbex affiliate company and contributors
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package com.codbex.atlas.ui.tests;
 
 import com.codbex.atlas.ui.Atlas;
@@ -7,7 +18,7 @@ import org.junit.jupiter.api.Test;
 
 class HomePageIT extends UserInterfaceIntegrationTest {
 
-    private static final String ECLIPSE_DIRIGIBLE_HEADER = "Eclipse Dirigible";
+    private static final String CODBEX_HEADER = "codbex";
 
     private Atlas atlas;
 
@@ -20,5 +31,5 @@ class HomePageIT extends UserInterfaceIntegrationTest {
     void testOpenHomepage() {
         atlas.openHomePage();
 
-        browser.assertElementExistsByTypeAndText(HtmlElementType.HEADER5, ECLIPSE_DIRIGIBLE_HEADER);
+        browser.assertElementExistsByTypeAndText(HtmlElementType.HEADER5, CODBEX_HEADER);
     }}
