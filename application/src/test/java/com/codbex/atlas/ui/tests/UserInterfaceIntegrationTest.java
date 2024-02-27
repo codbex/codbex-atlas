02902b261f30b2c6d8c43216c9e431787fd2865d
MinaDoncheva <mina.doncheva@icloud.com>
2024-02-27 15:14:06 +0200

    implemented test framework module for integration testing

--- application/src/test/java/com/codbex/atlas/ui/tests/UserInterfaceIntegrationTest.java
+++ application/src/test/java/com/codbex/atlas/ui/tests/UserInterfaceIntegrationTest.java
@@ -12,21 +12,12 @@
 package com.codbex.atlas.ui.tests;
 
 import com.codbex.atlas.ui.IntegrationTest;
-import com.codbex.atlas.ui.framework.Browser;
-import com.codbex.atlas.ui.framework.BrowserImpl;
-import org.junit.jupiter.api.BeforeEach;
-import org.springframework.boot.test.web.server.LocalServerPort;
+import org.eclipse.dirigible.tests.framework.Browser;
+import org.springframework.beans.factory.annotation.Autowired;
 
 abstract class UserInterfaceIntegrationTest extends IntegrationTest {
 
-    @LocalServerPort
-    private int localServerPort;
-
+    @Autowired
     protected Browser browser;
-
-    @BeforeEach
-    final void initBrowser() {
-        this.browser = new BrowserImpl(localServerPort);
-    }
 
 }
