a5ad76feef53d42fbd0912b64feaac45ecb55fc1
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-03-06 10:25:02 +0200

    Adapt tests to latest framework changes in Dirigible

---
+++ application/src/test/java/com/codbex/atlas/ui/tests/TestConfigurations.java
@@ -0,0 +1,9 @@
+package com.codbex.atlas.ui.tests;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages = "org.eclipse.dirigible")
+@TestConfiguration
+class TestConfigurations {
+}
