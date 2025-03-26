c2e219e0484b07432e709d82633cae2ba5aa50b1
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-03-26 16:53:03 +0200

    Add more integration tests from dirigible

--- application/src/main/java/com/codbex/atlas/AtlasApplication.java
+++ application/src/main/java/com/codbex/atlas/AtlasApplication.java
@@ -16,6 +16,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -25,6 +26,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication(scanBasePackages = {"com.codbex.atlas", "org.eclipse.dirigible.components"},
         exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class,
                 HibernateJpaAutoConfiguration.class, JdbcTemplateAutoConfiguration.class})
+@ComponentScan({"org.eclipse.dirigible", "com.codbex.atlas"})
 @EnableScheduling
 public class AtlasApplication {
     private static long startedAt;
