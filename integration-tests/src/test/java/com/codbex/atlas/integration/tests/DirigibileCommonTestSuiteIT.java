c02cf52cdfbf92f562a29d6112818dea0d3f145d
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-03-27 09:14:48 +0200

    formatting

--- integration-tests/src/test/java/com/codbex/atlas/integration/tests/DirigibileCommonTestSuiteIT.java
+++ integration-tests/src/test/java/com/codbex/atlas/integration/tests/DirigibileCommonTestSuiteIT.java
@@ -11,31 +11,30 @@ import org.junit.platform.suite.api.Suite;
 
 @Suite
 @SelectClasses({//
-        ApproveLeaveRequestBpmIT.class,//
-        BPMStarterTemplateIT.class,//
-        CamelDirigibleJavaScriptComponentCronRouteIT.class,//
-        CamelDirigibleJavaScriptComponentHttpRouteIT.class,//
-        CamelDirigibleTwoStepsJSInvokerCronRouteIT.class,//
-        CamelDirigibleTwoStepsJSInvokerHttpRouteIT.class,//
-        CamelExtractTransformLoadJdbcIT.class,//
-        CamelExtractTransformLoadTypescriptIT.class,//
-        CmsSuiteIT.class,//
-        CreateNewProjectIT.class,//
-        CsvimIT.class,//
-        CustomSecurityIT.class,//
-        DatabasePerspectiveIT.class,//
-        DeclineLeaveRequestBpmIT.class,//
-        GitPerspectiveIT.class,//
-        HomepageRedirectIT.class,//
-        MailIT.class,//
-        MessagingFacadeIT.class,//
-        MultitenancyIT.class,//
-        ODataAPIIT.class,//
-        SecurityIT.class,//
+        ApproveLeaveRequestBpmIT.class, //
+        BPMStarterTemplateIT.class, //
+        CamelDirigibleJavaScriptComponentCronRouteIT.class, //
+        CamelDirigibleJavaScriptComponentHttpRouteIT.class, //
+        CamelDirigibleTwoStepsJSInvokerCronRouteIT.class, //
+        CamelDirigibleTwoStepsJSInvokerHttpRouteIT.class, //
+        CamelExtractTransformLoadJdbcIT.class, //
+        CamelExtractTransformLoadTypescriptIT.class, //
+        CmsSuiteIT.class, //
+        CreateNewProjectIT.class, //
+        CsvimIT.class, //
+        CustomSecurityIT.class, //
+        DatabasePerspectiveIT.class, //
+        DeclineLeaveRequestBpmIT.class, //
+        GitPerspectiveIT.class, //
+        HomepageRedirectIT.class, //
+        MailIT.class, //
+        MessagingFacadeIT.class, //
+        MultitenancyIT.class, //
+        ODataAPIIT.class, //
+        SecurityIT.class, //
         TerminalIT.class//
 })
 public class DirigibileCommonTestSuiteIT {
     // use this suite class to run tests in specific order if needed
     // it is not configured to be executed automatically by the maven plugins
 }
-
