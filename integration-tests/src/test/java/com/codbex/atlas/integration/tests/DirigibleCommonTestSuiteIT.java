db8a721b754239b21331aec54913f3638ffadc5f
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-06-04 14:39:01 +0300

    register the new camel project template ITs

--- integration-tests/src/test/java/com/codbex/atlas/integration/tests/DirigibleCommonTestSuiteIT.java
+++ integration-tests/src/test/java/com/codbex/atlas/integration/tests/DirigibleCommonTestSuiteIT.java
@@ -23,6 +23,8 @@ import org.junit.platform.suite.api.Suite;
 @SelectClasses({//
         ApproveLeaveRequestBpmIT.class, //
         BPMStarterTemplateIT.class, //
+        CamelCronRouteStarterTemplateIT.class, //
+        CamelHttpRouteStarterTemplateIT.class, //
         CamelDirigibleJavaScriptComponentCronRouteIT.class, //
         CamelDirigibleJavaScriptComponentHttpRouteIT.class, //
         CamelDirigibleTwoStepsJSInvokerCronRouteIT.class, //
@@ -44,5 +46,5 @@ import org.junit.platform.suite.api.Suite;
         SecurityIT.class, //
         TerminalIT.class//
 })
-public class DirigibileCommonTestSuiteIT {
+public class DirigibleCommonTestSuiteIT {
 }
